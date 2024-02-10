7da71bcc7bb31716c23df8783ff5a58a5bf936fc
javigondeveloper <javiergonzalez.webdeveloper@gmail.com>
2024-02-10 22:25:44 +0100

    i18n refactoring

---
+++ src/i18n.ts
@@ -0,0 +1,30 @@
+export const i18n = {
+  es: {
+    themeMode: {
+      dark: 'modo oscuro',
+      light: 'modo claro',
+    },
+    download: 'descargar CV',
+    print: 'imprimir CV',
+    language: 'español',
+    emailTitle: `Enviar un correo electrónico a la dirección `,
+    phoneTitle: `Llamar por teléfono al número `,
+    profilesTitle: `Visitar el perfil en `,
+    project: `Ver el proyecto `,
+    gitHubProject: 'visitar el gitHub del proyecto ',
+  },
+  en: {
+    themeMode: {
+      dark: 'dark mode',
+      light: 'light mode',
+    },
+    download: 'download CV',
+    print: 'print CV',
+    language: 'english',
+    emailTitle: `Send email to the  address `,
+    phoneTitle: `Phone call to the number `,
+    profilesTitle: `Visit the profile in `,
+    project: `See the project `,
+    gitHubProject: 'visit the gitHub of the project ',
+  },
+};
